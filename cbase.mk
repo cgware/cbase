3f53dbf545e488a4f61f25ed480141d1ba090bed
Martynas Bielskus <bmartynas99@gmail.com>
2024-11-20 17:58:15 +0200

    make: cleanup includes and libs

--- cbase.mk
+++ cbase.mk
@@ -1,18 +1,18 @@
 CBASE_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
-CBASE_SRC := $(wildcard $(CBASE_DIR)src/*.c)
-CBASE_INC := $(wildcard $(CBASE_DIR)src/*.h)
-CBASE_INCLUDE := $(wildcard $(CBASE_DIR)include/*.h)
+CBASE_SRC := $(CBASE_DIR)src/
+CBASE_C := $(wildcard $(CBASE_SRC)*.c)
+CBASE_H := $(wildcard $(CBASE_SRC)*.h)
+CBASE_HEADERS := $(wildcard $(CBASE_DIR)include/*.h)
 
-CBASE_OUTDIR := $(BUILDDIR)bin/cbase/$(ARCH)-$(CONFIG)
+CBASE_OUTDIR := $(BUILDDIR)bin/$(ARCH)-$(CONFIG)/cbase
 CBASE_INTDIR := $(CBASE_OUTDIR)/int/
-CBASE_OBJ := $(patsubst $(CBASE_DIR)%.c,$(CBASE_INTDIR)%.o,$(CBASE_SRC))
+CBASE_OBJ := $(patsubst $(CBASE_SRC)%.c,$(CBASE_INTDIR)%.o,$(CBASE_C))
 CBASE_GCDA := $(patsubst %.o,%.gcda,$(CBASE_OBJ))
 
-CBASE_INCLUDES := -I$(CBASE_DIR)src/ -I$(CBASE_DIR)include/
+CBASE_INCLUDES := -I$(CBASE_DIR)include/
 
-CBASE_NAME := cbase.a
-CBASE := $(CBASE_OUTDIR)/$(CBASE_NAME)
+CBASE := $(CBASE_OUTDIR)/cbase.a
 
 .PHONY: cbase
 cbase: $(CBASE)
@@ -21,21 +21,22 @@ $(CBASE): $(CBASE_OBJ)
 	@mkdir -p $(@D)
 	@ar rcs $@ $(CBASE_OBJ)
 
-$(CBASE_INTDIR)%.o: $(CBASE_DIR)%.c $(CBASE_INC) $(CBASE_INCLUDE)
+$(CBASE_INTDIR)%.o: $(CBASE_SRC)%.c $(CBASE_H) $(CBASE_HEADERS)
 	@mkdir -p $(@D)
-	@$(TCC) -m$(BITS) -c $(CBASE_INCLUDES) $(CFLAGS) -o $@ $<
+	@$(TCC) -m$(BITS) -c -I$(CBASE_SRC) $(CBASE_INCLUDES) $(CFLAGS) -o $@ $<
 
-CBASE_TEST_SRC := $(wildcard $(CBASE_DIR)test/*.c)
-CBASE_TEST_INC := $(wildcard $(CBASE_DIR)test/*.h)
-CBASE_TEST_INCLUDE := $(CBASE_INCLUDE)
+CBASE_TEST_SRC := $(CBASE_DIR)test/
+CBASE_TEST_C := $(wildcard $(CBASE_TEST_SRC)*.c)
+CBASE_TEST_H := $(wildcard $(CBASE_TEST_SRC)*.h)
+CBASE_TEST_HEADERS := $(CBASE_HEADERS)
 
-CBASE_TEST_OUTDIR := $(BUILDIR)bin/cbase_test/$(ARCH)-$(CONFIG)
+CBASE_TEST_OUTDIR := $(BUILDIR)bin/$(ARCH)-$(CONFIG)/cbase_test
 CBASE_TEST_INTDIR := $(CBASE_TEST_OUTDIR)/int/
-CBASE_TEST_OBJ := $(patsubst $(CBASE_DIR)%.c,$(CBASE_TEST_INTDIR)%.o,$(CBASE_TEST_SRC))
+CBASE_TEST_OBJ := $(patsubst $(CBASE_TEST_SRC)%.c,$(CBASE_TEST_INTDIR)%.o,$(CBASE_TEST_C))
 CBASE_TEST_GCDA := $(patsubst %.o,%.gcda,$(CBASE_TEST_OBJ))
 
-CBASE_TEST_INCLUDES := -I$(CBASE_DIR)test/ -I$(CBASE_DIR)include/
-CBASE_TEST_LIBS := -L$(CBASE_OUTDIR) -l:$(CBASE_NAME)
+CBASE_TEST_INCLUDES :=  $(CBASE_INCLUDES)
+CBASE_TEST_LIBS := $(CBASE)
 
 CBASE_TEST := $(CBASE_TEST_OUTDIR)/cbase_test
 
@@ -44,10 +45,10 @@ cbase_test: $(CBASE_TEST)
 	@rm -rf $(CBASE_GCDA) $(CBASE_TEST_GCDA)
 	@$(CBASE_TEST)
 
-$(CBASE_TEST): $(CBASE) $(CBASE_TEST_OBJ)
+$(CBASE_TEST): $(CBASE_TEST_OBJ) $(CBASE_TEST_LIBS)
 	@mkdir -p $(@D)
-	@$(TCC) -m$(BITS) $(LDFLAGS) -o $@ $(CBASE_TEST_OBJ) $(CBASE_TEST_LIBS)
+	@$(TCC) -m$(BITS) $(LDFLAGS) -o $@ $(CBASE_TEST_OBJ) $(patsubst %,-L%,$(dir $(CBASE_TEST_LIBS))) $(patsubst %,-l:%,$(notdir $(CBASE_TEST_LIBS)))
 
-$(CBASE_TEST_INTDIR)%.o: $(CBASE_DIR)%.c $(CBASE_TEST_INC) $(CBASE_TEST_INCLUDE)
+$(CBASE_TEST_INTDIR)%.o: $(CBASE_TEST_SRC)%.c $(CBASE_TEST_H) $(CBASE_TEST_HEADERS)
 	@mkdir -p $(@D)
-	@$(TCC) -m$(BITS) -c $(CBASE_TEST_INCLUDES) $(CFLAGS) -o $@ $<
+	@$(TCC) -m$(BITS) -c -I$(CBASE_TEST_SRC) $(CBASE_TEST_INCLUDES) $(CFLAGS) -o $@ $<
