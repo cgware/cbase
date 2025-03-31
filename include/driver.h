2e5b9e419e8e31590df0498b184e8c099016443d
Martynas Bielskus <bmartynas99@gmail.com>
2025-03-31 15:00:06 +0300

    driver: fixes

--- include/driver.h
+++ include/driver.h
@@ -8,7 +8,8 @@ typedef struct driver_s {
 
 #if defined(_MSC_VER)
 	#define DRIVER(_name, _type, _data)                                                                                                \
-		__pragma(section(".drv$u")) __declspec(allocate(".drv$u")) driver_t _drv_##_name = {.type = _type, .data = _data}
+		__pragma(section(".drv$u", read)) __declspec(allocate(".drv$u"))                                                           \
+		__declspec(align(16)) driver_t _drv_##_name = {.type = _type, .data = _data}
 
 extern driver_t _drv_start;
 extern driver_t _drv_end;
@@ -17,8 +18,7 @@ extern driver_t _drv_end;
 	#define DRIVER_END   &_drv_end
 #else
 	#define DRIVER(_name, _type, _data)                                                                                                \
-		static driver_t _drv_##_name __attribute__((aligned(4))) __attribute__((unused))                                           \
-		__attribute__((__section__("drv"))) = {.type = _type, .data = _data}
+		driver_t _drv_##_name __attribute__((__section__("drv"))) __attribute__((aligned(16))) = {.type = _type, .data = _data}
 
 extern driver_t __start_drv[];
 extern driver_t __stop_drv[];
--- src/driver.c
+++ src/driver.c
@@ -1,8 +1,6 @@
 #include "driver.h"
 
 #if defined(_MSC_VER)
-	#pragma section(".drv$a")
-	#pragma section(".drv$z")
-__declspec(allocate(".drv$a")) driver_t _drv_start = {0};
-__declspec(allocate(".drv$z")) driver_t _drv_end   = {0};
+__pragma(section(".drv$a", read)) __declspec(allocate(".drv$a")) driver_t _drv_start = {0};
+__pragma(section(".drv$z", read)) __declspec(allocate(".drv$z")) driver_t _drv_end   = {0};
 #endif
